5307f520b319f9e7d2038ce17299b9194d772483
renzhendong <renzhendong@juwang.cn>
2020-02-22 22:18:16 +0800

    init

---
+++ lib/common/Global.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import '../models/index.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+const _themes = <MaterialColor>[
+  Colors.blue,
+  Colors.cyan,
+  Colors.teal,
+  Colors.green,
+  Colors.red,
+];
+class Global {
+  // 插件
+  static SharedPreferences _prefs;
+
+  // 用户信息
+  static Profile profile = Profile();
+
+  // 可选的主题列表
+  static List<MaterialColor> get themes => _themes;
+
+  static Future init() async {
+    _prefs = await SharedPreferences.getInstance();
+    var _profile = _prefs.getString("profie");
+    if (_profile != null) {
+      try {
+        profile = Profile.fromJson(jsonDecode(_profile));
+      } catch (e) {
+        print(e);
+      }
+    }
+  }
+  // 持久化Profile信息
+  static saveProfile() =>
+      _prefs.setString("profile", jsonEncode(profile.toJson()));
+}
---
+++ lib/models/user.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+User _$UserFromJson(Map<String, dynamic> json) {
+  return User()
+    ..name = json['name'] as String
+    ..email = json['email'] as String
+    ..address = json['address'] as String
+    ..age = json['age'] as num
+    ..login = json['login'] as String;
+}
+
+Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
+      'name': instance.name,
+      'email': instance.email,
+      'address': instance.address,
+      'age': instance.age,
+      'login': instance.login
+    };
